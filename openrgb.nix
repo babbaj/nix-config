edb29e623a34fe1727ac5a6073d6cbf6c2799d70
Babbaj <babbaj45@gmail.com>
2022-04-09 02:40:28 -0400

    update openrgb.nix

--- openrgb.nix
+++ openrgb.nix
@@ -2,7 +2,13 @@
 
 {
   systemd.services.rgb-server = {
-    script = "${pkgs.openrgb} --server";
+    script = "${pkgs.openrgb}/bin/openrgb --server";
     wantedBy = [ "multi-user.target" ]; 
   };
+
+  environment.systemPackages = with pkgs; [
+    openrgb
+    (writeScriptBin "rgb-white" "openrgb --color FF7878")
+    (writeScriptBin "rgb-off" "openrgb --color 0")
+  ];
 }
