1eeaaff139f24268286f2f5eeba99472181f9c99
Babbaj <babbaj45@gmail.com>
2022-05-20 21:35:52 -0400

    steam-proton linux only

--- home/steam-proton.nix
+++ home/steam-proton.nix
@@ -7,12 +7,14 @@ let
         sha256 = "sha256:1nvrbifsbgm2fz9114q3wyzdrm52jnjir3ncjc7inalmdymsmq4g";
     };
     path = "~/.steam/root/compatibilitytools.d/Proton-${version}";
+
+    inherit (pkgs.stdenv.hostPlatform) isLinux;
 in
 {
   # For some reason a normal symlink doesn't work and recursive symlinking on every activation is too slow
-  home.activation.proton-activation = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  home.activation.proton-activation = lib.mkIf isLinux (lib.hm.dag.entryAfter ["writeBoundary"] ''
     if [[ ! -d ${path} ]]; then
       cp -r --reflink=auto ${proton-ge} ${path}
     fi
-  '';
+  '');
 }
