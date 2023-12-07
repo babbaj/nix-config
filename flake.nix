fb4ce83054657d99c2a94c8f8616d510c4b5606b
Babbaj <babbaj45@gmail.com>
2023-12-07 02:54:13 -0500

    delete a bunch of old crap

--- flake.nix
+++ flake.nix
@@ -45,7 +45,6 @@
       name = "nixpkgs-patched";
       src = nixpkgs;
       patches = with pkgs; [
-        #./jetbrains-troll.patch
         #./fix-xserver.patch
         #(fetchpatch { # discord
         #  url = "https://github.com/NixOS/nixpkgs/commit/a859d764e9f9905b170152accb46fddc06b52028.patch";
--- mic-setup/mic-setup.nix
+++ mic-setup/mic-setup.nix
@@ -19,20 +19,6 @@ in
     '';
   };
 
-  environment.etc = {
-    "wireplumber/main.lua.d/90-load-custom-scrips.lua".text = ''
-      --load_script("link-easyeffects-to-proxy.lua")
-      --load_script("reroute-mic-for-cringe-programs.lua")
-      load_script("volume.lua")
-    '';
-    "wireplumber/scripts/link-easyeffects-to-proxy.lua".source = ./link-easyeffects-to-proxy.lua;
-    "wireplumber/scripts/reroute-mic-for-cringe-programs.lua".source = ./reroute-mic-for-cringe-programs.lua;
-    "wireplumber/scripts/volume.lua".source = ./volume.lua;
-
-    # TODO: inline this file
-    "pipewire/pipewire.conf.d/pw-loopback-nodes.conf".source = ./pw-loopback-nodes.conf;
-  };
-
   systemd.user.services.push-to-talk = {
     description = "Push to talk for specific pipewire nodes";
     after = [ "pipewire.service" ];
