01b145fdaa61bd16079e33d7a77cee1cb45775ec
Babbaj <babbaj45@gmail.com>
2024-03-10 23:16:48 -0400

    proton symlink bug seems to be fixed

--- home/steam-proton.nix
+++ home/steam-proton.nix
@@ -1,31 +1,26 @@
 { config, lib, pkgs, ... }:
 
 let
-    mkProtonGEScript = { version, sha256 }:
+    mkProtonGE = { version, sha256 }:
     let
      unpacked = fetchTarball {
         url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
         inherit sha256;
       };
-      path = "~/.steam/root/compatibilitytools.d/${version}";
+      path = ".steam/root/compatibilitytools.d/${version}";
     in
-    ''
-      if [[ ! -d ${path} ]]; then
-        cp -r --reflink=auto ${unpacked} ${path}
-      fi
-    '';
+    { ${path}.source = unpacked; };
 
     inherit (pkgs.stdenv.hostPlatform) isLinux;
 in
 {
-  # For some reason a normal symlink doesn't work and recursive symlinking on every activation is too slow
-  home.activation.proton-activation = lib.mkIf isLinux (lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${mkProtonGEScript { version = "Proton-7.0rc3-GE-1"; sha256 = "sha256:1nvrbifsbgm2fz9114q3wyzdrm52jnjir3ncjc7inalmdymsmq4g"; }}
-    ${mkProtonGEScript { version = "GE-Proton7-37"; sha256 = "sha256:0wgdp8vxpbi66fh4r7g4kvxbyfyqglzjmfgh8bm4wfns8ikwii9z"; }}
-    ${mkProtonGEScript { version = "GE-Proton7-55"; sha256 = "sha256:0szrza88ic0rx6y90y1s655faxfz7lq24315zw0xl107gvszw8p8"; }}
-    ${mkProtonGEScript { version = "GE-Proton8-2"; sha256 = "sha256:1n6zs00fngrbjp761drmrvr1gk8fn8x85npayyh70rfs72dmv6hc"; }}
-    ${mkProtonGEScript { version = "GE-Proton8-13"; sha256 = "sha256:0nj7m55hag0cvjs40lfsj3627gqlrknps5xdg8f2m1rmdhfgky65"; }}
-    ${mkProtonGEScript { version = "GE-Proton8-16"; sha256 = "sha256:0r11sf7pljw5rqlgbnkl6lkw2cpqyvd16vjp8f64hqjx4ma3947g"; }}
-    ${mkProtonGEScript { version = "GE-Proton9-1"; sha256 = "sha256:1zc1c1scqnpxsfxj6micpgvn317k7gd48aya8m3c5v6nbi377nm1"; }}
-  '');
+  home.file = lib.mkIf isLinux (lib.mkMerge [
+    (mkProtonGE { version = "Proton-7.0rc3-GE-1"; sha256 = "sha256:1nvrbifsbgm2fz9114q3wyzdrm52jnjir3ncjc7inalmdymsmq4g"; })
+    (mkProtonGE { version = "GE-Proton7-37"; sha256 = "sha256:0wgdp8vxpbi66fh4r7g4kvxbyfyqglzjmfgh8bm4wfns8ikwii9z"; })
+    (mkProtonGE { version = "GE-Proton7-55"; sha256 = "sha256:0szrza88ic0rx6y90y1s655faxfz7lq24315zw0xl107gvszw8p8"; })
+    (mkProtonGE { version = "GE-Proton8-2"; sha256 = "sha256:1n6zs00fngrbjp761drmrvr1gk8fn8x85npayyh70rfs72dmv6hc"; })
+    (mkProtonGE { version = "GE-Proton8-13"; sha256 = "sha256:0nj7m55hag0cvjs40lfsj3627gqlrknps5xdg8f2m1rmdhfgky65"; })
+    (mkProtonGE { version = "GE-Proton8-16"; sha256 = "sha256:0r11sf7pljw5rqlgbnkl6lkw2cpqyvd16vjp8f64hqjx4ma3947g"; })
+    (mkProtonGE { version = "GE-Proton9-1"; sha256 = "sha256:1zc1c1scqnpxsfxj6micpgvn317k7gd48aya8m3c5v6nbi377nm1"; })
+  ]);
 }
