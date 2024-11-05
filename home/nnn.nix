c2d74afab59c0a813141facd8f685afc9292fa4c
Babbaj <babbaj45@gmail.com>
2024-11-05 14:52:24 -0500

    a bunch of bs to make my nnn config work with the desktop entry

--- home/nnn.nix
+++ home/nnn.nix
@@ -1,55 +1,39 @@
 { pkgs, ... }:
 
 {
-  programs.nnn = {
-    enable = true;
-    plugins = {
-      mappings = {
-        #f = "finder";
-        o = "fzopen";
-        p = "preview-tui";
-        d = "dragdrop";
-        z = "fzplug";
-      };
-    };
-    extraPackages = with pkgs; [ xdragon ];
-    package = (pkgs.nnn.override({ withNerdIcons = true; })).overrideAttrs({...}: { patches = [ ../nnn-patch.diff ]; });
-  };
+  home.packages = let
+    patched = (pkgs.nnn.override({ withNerdIcons = true; }))
+      .overrideAttrs({...}: { patches = [ ../nnn-patch.diff ]; });
+    wrapped = pkgs.writeShellScriptBin "nnn" ''
+      export NNN_PLUG="f:finder;o:fzopen;p:preview-tui;d:dragdrop;z:fzplug"
+      ${patched}/bin/nnn -a -e -P p "$@"
+    '';
+    unwrapped = pkgs.writeShellScriptBin "nnn-unwrapped" ''
+      ${patched}/bin/nnn "$@"
+    '';
+  in
+  [ wrapped unwrapped ];
 
   programs.zsh.shellAliases = {
-    n = "NNN_FIFO=/tmp/nnn.fifo nquitcd -e -P p";
+    n = "nnn";
   };
 
   programs.zsh.initExtra = ''
-    nquitcd ()
-    {
-        # Block nesting of nnn in subshells
-        [ "''${NNNLVL:-0}" -eq 0 ] || {
-            echo "nnn is already running"
-            return
-        }
+    nnn () {
+      # Block nesting of nnn in subshells
+      [ "''${NNNLVL:-0}" -eq 0 ] || {
+          echo "nnn is already running"
+          return
+      }
 
-        # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
-        # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
-        # see. To cd on quit only on ^G, remove the "export" and make sure not to
-        # use a custom path, i.e. set NNN_TMPFILE *exactly* as follows:
-        #      NNN_TMPFILE="''${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
-        export NNN_TMPFILE="''${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+      export NNN_TMPFILE="''${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
 
-        # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
-        # stty start undef
-        # stty stop undef
-        # stty lwrap undef
-        # stty lnext undef
+      command nnn "$@"
 
-        # The command builtin allows one to alias nnn to n, if desired, without
-        # making an infinitely recursive alias
-        command nnn "$@"
-
-        [ ! -f "$NNN_TMPFILE" ] || {
-            . "$NNN_TMPFILE"
-            rm -f -- "$NNN_TMPFILE" > /dev/null
-        }
+      [ ! -f "$NNN_TMPFILE" ] || {
+          . "$NNN_TMPFILE"
+          rm -f -- "$NNN_TMPFILE" > /dev/null
+      }
     }
   '';
 }
